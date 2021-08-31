816c57e6d85eaa449d20d6a792486d0c27b4978e
Johnny Qian <johnnyqian@Johnnys-MacBook-Air.local>
2021-08-31 20:06:01 +1000

    Revert "initialised routes"
    This reverts commit 1c3855a5bbc64a09897394b5b6056ad66dc70320.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import Timeline from './components/pages/TimelineView/Timeline';
-// import ContactView from './components/pages/ContactView/ContactView';
-import Home from './components/pages/Home';
+import logo from './logo.svg';
+import './App.css';
+
+// // Firebase App (the core Firebase SDK) is always required and must be listed first
+// import firebase from "firebase/app";
+// // If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// // import * as firebase from "firebase/app"
+
+// // If you enabled Analytics in your project, add the Firebase SDK for Analytics
+// import "firebase/analytics";
+
+// // Add the Firebase products that you want to use
+// import "firebase/auth";
+// import "firebase/firestore";
+
 
 function App() {
   return (
-    <div className='App'>
-      <p>I am a page!</p>
-      <Router>
-        <Switch>
-          <Route path='/home' component={Home} />
-          <Route path='/timeline' component={Timeline} />
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
