e9017abbcdad5f3504a235377f75acf479ad0b6f
JOCHEN691238 <82068738+JOCHEN691238@users.noreply.github.com>
2021-09-04 15:17:06 +1000

    Revert "Update server.js"
    This reverts commit 05ab3c0b32a5f14b1a93bcb74f920e43b8410ab5.

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDhD0hAiSlMjG9Y-3RstsFaAzdgELhJ3Bk",
-  authDomain: "xxthecalvinsxx-f8d6f.firebaseapp.com",
-  projectId: "xxthecalvinsxx-f8d6f",
-  storageBucket: "xxthecalvinsxx-f8d6f.appspot.com",
-  messagingSenderId: "162288928335",
-  appId: "1:162288928335:web:6a27e1669be5255e570c3a",
-  measurementId: "G-N2PJLHYBE0"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const app = initializeApp(firebaseConfig);
 const express = require('express');
 
 const app = express();
